c82741eec4fd0ba9849253fdd54da4b1661af29f
huliqun <huliquns@126.com>
2018-09-18 14:37:57 +0800

    fix

--- puppet/client.py
+++ puppet/client.py
@@ -425,7 +425,7 @@ class Client:
             else:
                 r = user32.SendMessageW(hEdit, MSG['WM_SETTEXT'], 0, text)
                 sr = user32.SendMessageW(hEdit, MSG['WM_SETFOCUS'], 0, 0) # for huatai
-                # sr = user32.SendMessageW(hEdit, MSG['WM_KILLFOCUS'], 0, 0) # for huatai
+                sr = user32.SendMessageW(hEdit, MSG['WM_KILLFOCUS'], 0, 0) # for huatai
                 
         return self
 
